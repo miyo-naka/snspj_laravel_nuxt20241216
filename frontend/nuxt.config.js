8f0431ac993e062db89203e77d343737113f8932
miyokonakada <fortissimo_pikkoro@yahoo.co.jp>
2024-12-24 08:31:49 +1000

    update before fontawesome

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -18,12 +18,6 @@ export default {
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-    script: [
-      {
-        src: "https://kit.fontawesome.com/8f10bc7fb3.js",
-        crossorigin: "anonymous",
-      },
-    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
